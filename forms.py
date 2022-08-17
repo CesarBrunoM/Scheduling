a25191e5ba9e355d3969b105338d5f520fad6a9a
CesarBrunoM <58617507+CesarBrunoM@users.noreply.github.com>
2022-08-16 23:48:53 -0300

    Versão 1.1
    Ajuste nas validações dos formulários e estilos das paginas.

--- forms.py
+++ forms.py
@@ -5,21 +5,22 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
 
 class FormCriarConta():
     usuario = SubmitField('Nome usuário', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
     confirma_senha = SubmitField('Confirmação senha', DataRequired(), EqualTo('senha'))
     botao_submit_criar = SubmitField('Concluir')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembra_acesso = BooleanField('Lembrar acesso')
     botao_submit_login = SubmitField('Entrar')
 
 
 class SolicitacaoCadastro(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
     whatsapp = StringField('Whatsapp',
-                           validators=[Regexp(regex=r"^\d{3}[-]{1}\d{3}[-]{1}\d{4}$", message='Numero incorreto!'), Length(11,11)])
+                           validators=[DataRequired(),
+                                       Length(11, 11)])
     botao_submit_enviar = SubmitField('Enviar')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, SolicitacaoCadastro
 
 app = Flask(__name__)
@@ -7,24 +7,32 @@ lista_clientes = ['Bruno Cesar', 'Luana Lorrane', 'Haline Melo']
 
 app.config['SECRET_KEY'] = '6025524ac8f6491f7d860090f8e77a47'
 
+
 @app.route("/")
 def home():
     return render_template('home.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_solicitarcadastro = SolicitacaoCadastro()
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Seja bem vindo {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if form_solicitarcadastro.validate_on_submit() and 'botao_submit_enviar' in request.form:
+        flash(f'Informações encaminhadas! Logo faremos parte do mesmo time.', 'alert-primary')
+
     return render_template('login.html', form_login=form_login, form_solicitarcadastro=form_solicitarcadastro)
 
 
-@app.route("/perfil")
+@app.route("/perfil", methods=['GET', 'POST'])
 def perfil():
     return render_template('perfil.html')
 
 
-@app.route("/atendimento")
+@app.route("/atendimento", methods=['GET', 'POST'])
 def atendimento():
     return render_template('atendimentos.html')
 
