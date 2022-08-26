15676bd71024947d57d56e4f0721edcc377d70cf
CesarBrunoM <58617507+CesarBrunoM@users.noreply.github.com>
2022-08-25 22:20:09 -0300

    Alteração do nome INDOC
    Alteração do nome do sistema para o definido anteriormente.

--- forms.py
+++ forms.py
@@ -1,25 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
-class FormCriarConta():
-    usuario = SubmitField('Nome usuário', validators=[DataRequired()])
+class FormCriarConta(FlaskForm):
+    username = SubmitField('Nome usuário', validators=[DataRequired()])
+    nome_completo = StringField('Nome completo', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
     confirma_senha = SubmitField('Confirmação senha', DataRequired(), EqualTo('senha'))
+    numerocontato = StringField('Nº Celular', validators=[DataRequired(), Length(12, 12)])
     botao_submit_criar = SubmitField('Concluir')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
+    email = StringField('Usuário', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembra_acesso = BooleanField('Lembrar acesso')
     botao_submit_login = SubmitField('Entrar')
 
 
 class SolicitacaoCadastro(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
+    emailrequerido = StringField('Email', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
     whatsapp = StringField('Whatsapp',
                            validators=[DataRequired(),
                                        Length(11, 11)])
