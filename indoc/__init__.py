c64f727d22a6807b61828d87859e6d5918a72951
Bruno Cesar Melo de Lima <58617507+CesarBrunoM@users.noreply.github.com>
2022-09-10 23:47:28 -0300

    Models Alterações
    Criação de mais tabelas do Models.

--- indoc/__init__.py
+++ indoc/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6025524ac8f6491f7d860090f8e77a47'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/systemdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/systembd'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
--- indoc/models.py
+++ indoc/models.py
@@ -13,10 +13,46 @@ class Usuario(database.Model):
     data_nascimento = database.Column(database.DateTime)
     data_criacao = database.Column(database.DateTime, default=datetime.utcnow)
     cargo = database.Column(database.String(25), default='Não Informado')
-    #id_profissao = database.Column(database.Integer, database.ForeignKey('profissao.id_profissao'), nullable=False)
 
 
-#class Profissao(database.Model):
- #   id_profissao = database.Column(database.Integer, primary_key=True)
-  #  profissao_nome = database.Column(database.String(25), nullable=False)
-   # usuario = database.relationship('UsuarioLogin', backref='profissao', lazy=True)
+class Cliente(database.Model):
+    id_cliente = database.Column(database.Integer, primary_key=True)
+    nome = database.Column(database.String(70))
+    contato = database.Column(database.String(13))
+    id_empresa = database.Column(database.Integer, database.ForeignKey('clienteempresa.id_empresa'))
+    atendimento = database.relationship('Cliente', backref='atendimentos', lazy=True)
+    # id_profissao = database.Column(database.Integer, database.ForeignKey('profissao.id_profissao'), nullable=False)
+    # usuario = database.relationship('UsuarioLogin', backref='profissao', lazy=True)
+
+
+class Clienteempresa(database.Model):
+    id_empresa = database.Column(database.Integer, primary_key=True)
+    razao = database.Column(database.String(100))
+    nome = database.Column(database.String(100))
+    cliente = database.relationship('Cliente', backref='clienteempresa', lazy=True)
+
+
+class Setor(database.Model):
+    id_setor = database.Column(database.Integer, primary_key=True)
+    nome = database.Column(database.String(70))
+    atendimento = database.relationship('Atendimentos', backref='setor', lazy=True)
+
+
+class Problemas(database.Model):
+    id_problema = database.Column(database.Integer, primary_key=True)
+    descricao = database.Column(database.String(200), nullable=True)
+    atendimentos = database.relationship('Atendimentos', backref='problemas', lazy=True)
+
+
+class Atendimentos(database.Model):
+    id_atendimento = database.Column(database.Integer, primary_key=True)
+    protocolo = database.Column(database.String(20), nullable=False)
+    data_cadastro = database.Column(database.DateTime, nullable=False)
+    data_inicio = database.Column(database.DateTime, nullable=False)
+    data_vencimento = database.Column(database.DateTime, nullable=False)
+    prioridade = database.Column(database.String(20), nullable=False)
+    local = database.Column(database.String(20), nullable=False)
+    observacao = database.Column(database.String(300))
+    problema = database.Column(database.String(200), nullable=False)
+    id_cliente = database.Column(database.Integer, database.ForeignKey('cliente.id_cliente'), nullable=False)
+    id_setor = database.Column(database.Integer, database.ForeignKey('setor.id_setor'), nullable=False)
