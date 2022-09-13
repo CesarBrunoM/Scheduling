888391a67ca389a9f2c473fd8e4bd5fd7b9322f1
CesarBrunoM <58617507+CesarBrunoM@users.noreply.github.com>
2022-09-13 00:23:35 -0300

    Conclusão Login
    Conclusão de validações login.

--- indoc/__init__.py
+++ indoc/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6025524ac8f6491f7d860090f8e77a47'
@@ -10,5 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/sys
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'alert-info'
+login_manager.login_message = 'Necessário fazer login para acessar a pagina.'
 
 from indoc import routes
--- indoc/routes.py
+++ indoc/routes.py
@@ -2,7 +2,7 @@ from indoc import app, bcrypt
 from flask import render_template, redirect, flash, url_for, request
 from indoc.forms import FormLogin, SolicitacaoCadastro, FormCriarConta
 from indoc.models import Usuario, database
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 lista_clientes = ['Bruno Cesar', 'Luana Lorrane', 'Haline Melo']
 
@@ -22,7 +22,11 @@ def login():
         if user and bcrypt.check_password_hash(user.senha, form_login.senha.data):
             login_user(user, remember=form_login.lembra_acesso.data)
             flash(f'Seja bem vindo {user.username}', 'alert-success')
-            return redirect(url_for('home'))
+            par_next = request.args.get('next')
+            if par_next:
+                return redirect(par_next)
+            else:
+                return redirect(url_for('home'))
         else:
             flash('Falha no login, verifique seu email e senha.', 'alert-danger')
     if form_solicitarcadastro.validate_on_submit() and 'botao_submit_enviar' in request.form:
@@ -31,28 +35,31 @@ def login():
     return render_template('login.html', form_login=form_login, form_solicitarcadastro=form_solicitarcadastro)
 
 
-@app.route("/perfil", methods=['GET', 'POST'])
+@app.route("/perfil/", methods=['GET', 'POST'])
+@login_required
 def perfil():
     return render_template('perfil.html')
 
 
 @app.route("/atendimento", methods=['GET', 'POST'])
+@login_required
 def atendimento():
     return render_template('atendimentos.html')
 
 
-@app.route("/usuarios", methods=['GET', 'POST'])
+@app.route("/usuario", methods=['GET', 'POST'])
+@login_required
 def usuario():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit() and 'botao_submit_criar' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
-        usuario = Usuario(username=form_criarconta.username.data,
-                          nome_completo=form_criarconta.nome_completo.data,
-                          email=form_criarconta.email.data,
-                          telefone=form_criarconta.telefone.data,
-                          senha=senha_cript,
-                          cargo=form_criarconta.cargo.data)
-        database.session.add(usuario)
+        user = Usuario(username=form_criarconta.username.data,
+                       nome_completo=form_criarconta.nome_completo.data,
+                       email=form_criarconta.email.data,
+                       telefone=form_criarconta.telefone.data,
+                       senha=senha_cript,
+                       cargo=form_criarconta.cargo.data)
+        database.session.add(user)
         database.session.commit()
         redirect(url_for('home'))
         flash(f'Usuário {form_criarconta.username.data} cadastrado com sucesso', 'alert-success')
@@ -60,5 +67,14 @@ def usuario():
 
 
 @app.route("/cliente")
+@login_required
 def cliente():
     return render_template('clientes.html', lista_clientes=lista_clientes)
+
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash('Sua sessão foi encerrada.', 'alert-success')
+    return redirect('login')
