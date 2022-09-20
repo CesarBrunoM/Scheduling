1c228dd800bfe3cbc5956b03470d401d13e27c66
CesarBrunoM <58617507+CesarBrunoM@users.noreply.github.com>
2022-09-20 01:49:31 -0300

    Edições Usuarios
    Alterações nos formulários de cadastro de usuários.

--- indoc/forms.py
+++ indoc/forms.py
@@ -1,17 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, PasswordField, BooleanField
+from wtforms import SubmitField, StringField, PasswordField, BooleanField, DateField, TelField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from indoc.models import Usuario
+
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome usuário', validators=[DataRequired()])
     nome_completo = StringField('Nome completo', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
-    confirma_senha = PasswordField('Confirmação senha', validators=[DataRequired(), EqualTo('senha')])
-    telefone = StringField('Nº Celular', validators=[DataRequired(), Length(11, 11)])
+    confirma_senha = PasswordField('Confirmação senha', validators=[DataRequired(), EqualTo('senha',
+                                                                                            message='Os campos de senha devem ser iguais.')])
+    telefone = TelField('Nº Celular', validators=[DataRequired(), Length(11, 11)])
+    foto_perfil = StringField('Foto perfil')
+    data_nascimento = DateField('Data Nascimento', validators=[DataRequired()])
     cargo = StringField('Cargo')
-    botao_submit_criar = SubmitField('Gravar')
+    botao_submit_criar = SubmitField('Confirmar')
 
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
@@ -32,3 +36,17 @@ class SolicitacaoCadastro(FlaskForm):
                            validators=[DataRequired(),
                                        Length(11, 11)])
     botao_submit_enviar = SubmitField('Enviar')
+
+
+class FormEditarPerfil(FlaskForm):
+    username = StringField('Nome usuário', validators=[DataRequired()])
+    nome_completo = StringField('Nome completo', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email(message='Endereço de email invalido!')])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
+    confirma_senha = PasswordField('Confirmação senha', validators=[DataRequired(), EqualTo('senha',
+                                                                                            message='Os campos de senha devem ser iguais.')])
+    telefone = TelField('Nº Celular', validators=[DataRequired(), Length(11, 11)])
+    foto_perfil = StringField('Foto perfil')
+    data_nascimento = DateField('Data Nascimento', validators=[DataRequired()])
+    cargo = StringField('Cargo')
+    botao_submit_editarperfil = SubmitField('Confirmar')
